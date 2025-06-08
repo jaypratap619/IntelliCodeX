e8715986ab8ea730dda3e83053d53ecd9fda4180
jay bhadoriya <18301@iiitu.ac.in>
2025-06-08 23:13:45 +0530

    UI components folder restructuring

--- frontend/src/pages/Sandbox.tsx
+++ frontend/src/pages/Sandbox.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import CodeEditor from "../components/Editor/CodeEditor"
-import FileTree from "../components/Editor/FileTree"
-import Preview from "../components/Editor/Preview"
+import FileTree from "../components/FileTree/FileTree"
+import Preview from "../components/Preview/Preview"
 
 const Sandbox: React.FC = () => {
   return (
